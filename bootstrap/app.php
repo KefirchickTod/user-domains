6a26c014dec0e50ae3c0cf6af4195f6f824431bc
zpyly <zacharpu2@gmail.com>
2026-02-26 01:43:46 +0200

    feat: deploy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Trust all proxies (required for Railway / any PaaS behind a load balancer)
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
